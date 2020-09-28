288d5e919b10f04443d3291f815fcf162093c302
Profx Boboma <69331713+profx-boboma@users.noreply.github.com>
2020-09-28 14:50:17 -0400

    Update macro.lua

--- macro.lua
+++ macro.lua
@@ -1,3 +1,5 @@
 -- Loop through all players in raid, and ceheck for a specific buff
 -- Check myself for a buff
 -- Interface for setting different consumables
+-- Baby steps: Print all of target's buffs:
+/run  for i=1,40 do local n,a,b,c,d,e,f,g,h,i,j,k,l,m = UnitBuff("target", i); if n then print(n,a,b,c,d,e,f,g,h,i,j,k,l,m) end end
